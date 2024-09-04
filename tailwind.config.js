e057f63ec549586de1184c06f4f3e25bf68150ad
Edan <ewstasiuk03@gmail.com>
2024-09-03 23:13:46 -0700

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'background': '#1E1E1E',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
