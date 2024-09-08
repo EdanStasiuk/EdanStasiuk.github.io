ad0122cbf2f38dbce23886c427d87412b3773942
Edan <ewstasiuk03@gmail.com>
2024-09-07 22:07:32 -0700

    Add links to socials, move Hero into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { useState, useEffect } from 'react';
-import FirstName from './components/FirstName';
-import LastName from './components/LastName';
-import { FaGithub } from 'react-icons/fa6';
-import { FaLinkedin } from 'react-icons/fa';
-import { FiMail } from 'react-icons/fi';
-import FirstNameBars from './components/FirstNameBars';
-import LastNameBars from './components/LastNameBars';
 import AboutMe from './components/AboutMe';
 import TechStack from './components/TechStack';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   const [isLowPixelRatio] = useState(true);
@@ -44,85 +38,7 @@ function App() {
     <div>
       <Navbar />
       <div id="top" className={`px-[10%] ${browserWidth >= browserWidthThreshold ? "pt-[150px]" : "pt-[50px]"}`}>
-        <div className="flex flex-col items-center lg:flex-row lg:justify-center lg:whitespace-nowrap">
-          <div
-            className={`${
-              browserWidth >= browserWidthThreshold ? 'flex' : 'hidden'
-            } ${isLowPixelRatio ? '' : 'scale-[90%]'} justify-center whitespace-nowrap lg:block`}
-          >
-            <div>
-              {isLowPixelRatio ? (
-                <FirstName
-                  browserWidth={browserWidth}
-                  browserWidthThreshold={browserWidthThreshold}
-                />
-              ) : (
-                <FirstNameBars />
-              )}
-              <div className="flex mt-8 items-end">
-                {isLowPixelRatio ? (
-                  <LastName
-                    browserWidth={browserWidth}
-                    browserWidthThreshold={browserWidthThreshold}
-                  />
-                ) : (
-                  <LastNameBars />
-                )}
-                <div
-                  className={`ml-8 relative ${isLowPixelRatio ? 'bottom-8' : 'top-4'}`}
-                >
-                  <div className="flex relative left-6 mb-6">
-                    <FaGithub className="scale-[400%] text-[#DF9CFF]" />
-                    <FaLinkedin className="scale-[425%] text-[#7AAFCE] ml-16" />
-                    <FiMail className="scale-[375%] text-[#D9D9D9] ml-16" />
-                  </div>
-                  <div className="text-[35px] mb-[1px] roboto-flex-italic-800 text-[#C1C1C1]">
-                    Software Engineer
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className={`${
-              browserWidth < browserWidthThreshold ? 'block' : 'hidden'
-            } flex flex-col items-center scale-[65%] lg:hidden`}
-          >
-            <div className="flex flex-col items-center whitespace-nowrap">
-              {isLowPixelRatio ? (
-                <>
-                  <FirstName
-                    browserWidth={browserWidth}
-                    browserWidthThreshold={browserWidthThreshold}
-                  />
-                  <div className="mb-4" />
-                  <LastName
-                    browserWidth={browserWidth}
-                    browserWidthThreshold={browserWidthThreshold}
-                  />
-                </>
-              ) : (
-                <>
-                  <FirstNameBars />
-                  <div className="mb-4" />
-                  <LastNameBars />
-                </>
-              )}
-              <div className="mb-16" />
-            </div>
-            <div className="flex flex-col items-center mt-8">
-              <div className="flex relative items-center mb-8">
-                <FaGithub className="scale-[500%] text-[#DF9CFF]" />
-                <FaLinkedin className="scale-[525%] ml-[84px] text-[#7AAFCE]" />
-                <FiMail className="scale-[475%] ml-[84px] text-[#D9D9D9]" />
-              </div>
-              <div className="text-[45px] whitespace-nowrap roboto-flex-italic-800 text-[#C1C1C1]">
-                Software Engineer
-              </div>
-            </div>
-          </div>
-        </div>
+        <Hero browserWidth={browserWidth} browserWidthThreshold={browserWidthThreshold} isLowPixelRatio={isLowPixelRatio} />
         <div
           className={`${
             browserWidth >= browserWidthThreshold ? 'mt-48' : 'mt-12'
---
+++ src/components/Hero.tsx
@@ -0,0 +1,132 @@
+import FirstName from '../components/FirstName';
+import LastName from '../components/LastName';
+import { FaGithub } from 'react-icons/fa6';
+import { FaLinkedin } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
+import FirstNameBars from '../components/FirstNameBars';
+import LastNameBars from '../components/LastNameBars';
+
+interface Props {
+  browserWidth: number;
+  browserWidthThreshold: number;
+  isLowPixelRatio: boolean;
+}
+
+export default function Hero({
+  browserWidth,
+  browserWidthThreshold,
+  isLowPixelRatio,
+}: React.PropsWithChildren<Props>) {
+  const handleMailClick = () => {
+    document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <div className="flex flex-col items-center lg:flex-row lg:justify-center lg:whitespace-nowrap">
+      <div
+        className={`${
+          browserWidth >= browserWidthThreshold ? 'flex' : 'hidden'
+        } ${isLowPixelRatio ? '' : 'scale-[90%]'} justify-center whitespace-nowrap lg:block`}
+      >
+        <div>
+          {isLowPixelRatio ? (
+            <FirstName
+              browserWidth={browserWidth}
+              browserWidthThreshold={browserWidthThreshold}
+            />
+          ) : (
+            <FirstNameBars />
+          )}
+          <div className="flex mt-8 items-end">
+            {isLowPixelRatio ? (
+              <LastName
+                browserWidth={browserWidth}
+                browserWidthThreshold={browserWidthThreshold}
+              />
+            ) : (
+              <LastNameBars />
+            )}
+            <div
+              className={`ml-8 relative ${isLowPixelRatio ? 'bottom-8' : 'top-4'}`}
+            >
+              <div className="flex relative left-6 mb-6">
+                <button
+                  onClick={() =>
+                    window.open('https://github.com/EdanStasiuk', '_blank')
+                  }
+                >
+                  <FaGithub className="scale-[400%] text-[#DF9CFF]" />
+                </button>
+                <button
+                  className="ml-16"
+                  onClick={() =>
+                    window.open(
+                      'https://linkedin.com/in/edan-stasiuk',
+                      '_blank'
+                    )
+                  }
+                >
+                  <FaLinkedin className="scale-[425%] text-[#7AAFCE]" />
+                </button>
+                <button className="ml-16" onClick={handleMailClick}>
+                  <FiMail className="scale-[375%] text-[#D9D9D9]" />
+                </button>
+              </div>
+              <div className="text-[35px] mb-[1px] roboto-flex-italic-800 text-[#C1C1C1]">
+                Software Engineer
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`${
+          browserWidth < browserWidthThreshold ? 'block' : 'hidden'
+        } flex flex-col items-center scale-[65%] lg:hidden`}
+      >
+        <div className="flex flex-col items-center whitespace-nowrap">
+          {isLowPixelRatio ? (
+            <>
+              <FirstName
+                browserWidth={browserWidth}
+                browserWidthThreshold={browserWidthThreshold}
+              />
+              <div className="mb-4" />
+              <LastName
+                browserWidth={browserWidth}
+                browserWidthThreshold={browserWidthThreshold}
+              />
+            </>
+          ) : (
+            <>
+              <FirstNameBars />
+              <div className="mb-4" />
+              <LastNameBars />
+            </>
+          )}
+          <div className="mb-16" />
+        </div>
+        <div className="flex flex-col items-center mt-8">
+          <div className="flex relative items-center mb-8">
+            <button onClick={() => window.open('https://github.com', '_blank')}>
+              <FaGithub className="scale-[500%] text-[#DF9CFF]" />
+            </button>
+            <button
+              className="ml-[84px]"
+              onClick={() => window.open('https://linkedin.com', '_blank')}
+            >
+              <FaLinkedin className="scale-[525%] text-[#7AAFCE]" />
+            </button>
+            <button className="ml-[84px]" onClick={handleMailClick}>
+              <FiMail className="scale-[475%] text-[#D9D9D9]" />
+            </button>
+          </div>
+          <div className="text-[45px] whitespace-nowrap roboto-flex-italic-800 text-[#C1C1C1]">
+            Software Engineer
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
