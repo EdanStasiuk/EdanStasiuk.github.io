04fcb38c84e4938d41c4005f04c72ecbd7e279ea
Edan <ewstasiuk03@gmail.com>
2024-09-11 20:58:36 -0700

    Add navbar responsiveness to browser size and scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,23 @@ import { BROWSER_WIDTH_THRESHOLD } from '../Utils/constants';
 
 export default function Navbar() {
   const [scrollPos, setScrollPos] = useState(0.0001);
+  const [padding, setPadding] = useState('py-4');
+  const [fontSize, setFontSize] = useState('text-[clamp(16px,2vw,24px)]');
   const { browserWidth } = useBrowserWidth();
 
   useEffect(() => {
     const handleScroll = () => {
       setScrollPos(window.scrollY);
+
+      // Update padding based on scroll position
+      const newPadding = window.scrollY > 75 ? '' : 'py-4';
+      setPadding(newPadding);
+
+      const newFontSize =
+        window.scrollY > 75
+          ? 'text-[clamp(16px,2vw,20px)]'
+          : 'text-[clamp(16px,2vw,24px)]';
+      setFontSize(newFontSize);
     };
 
     handleScroll();
@@ -43,18 +55,22 @@ export default function Navbar() {
 
   return (
     <div
-      className={`${browserWidth >= BROWSER_WIDTH_THRESHOLD - 500 ? "px-[10%]" : ""} sticky top-0 py-4 text-white z-10 bg-tails-background text-nowrap`}
+      className={`${browserWidth >= BROWSER_WIDTH_THRESHOLD - 500 ? 'px-[10%]' : ''} sticky top-0 ${padding} text-white z-10 bg-tails-background text-nowrap`}
       style={{
         borderBottom: '2px solid transparent',
         borderImage: `linear-gradient(to right, #343431 ${Math.max(0, colorStart)}%, #DF9CFF ${Math.max(0, colorEnd)}%, #343431 ${Math.min(100, colorEnd + 15)}%) 1`,
+        transition: 'padding 0.4s ease',
       }}
     >
       <div className={`flex justify-between items-center`}>
-        <nav className="flex font-robotoFlex font-thin text-[clamp(18px,2vw,24px)] w-full justify-between">
+        <nav
+          className={`flex font-robotoFlex font-thin ${fontSize} w-full justify-between text-wrap`}
+          style={{ transition: 'fontSize 0.4s ease' }}
+        >
           <div className="flex justify-center px-2">
             <a
               href="#top"
-              className="hover:text-[#DF9CFF] p-4 rounded"
+              className="text-center hover:text-[#DF9CFF] p-4 rounded"
               onClick={(e) => scrollToSection(e, 'top')}
             >
               Socials
@@ -63,7 +79,7 @@ export default function Navbar() {
           <div className="flex justify-center px-2">
             <a
               href="#about"
-              className="hover:text-[#DF9CFF] p-4 rounded"
+              className="text-center hover:text-[#DF9CFF] p-4 rounded"
               onClick={(e) => scrollToSection(e, 'about')}
             >
               About Me
@@ -72,7 +88,7 @@ export default function Navbar() {
           <div className="flex justify-center px-2">
             <a
               href="#stack"
-              className="hover:text-[#DF9CFF] p-4 rounded"
+              className="text-center hover:text-[#DF9CFF] p-4 rounded"
               onClick={(e) => scrollToSection(e, 'stack')}
             >
               Stack
@@ -81,7 +97,7 @@ export default function Navbar() {
           <div className="flex justify-center px-2">
             <a
               href="#projects"
-              className="hover:text-[#DF9CFF] p-4 rounded"
+              className="text-center hover:text-[#DF9CFF] p-4 rounded"
               onClick={(e) => scrollToSection(e, 'projects')}
             >
               Projects
